069e4aa17acc5130b8e7b766d069daf98d7698b9
José Ulises Niño Rivera <junr03@users.noreply.github.com>
2024-07-19 14:12:01 -0700

    Clean up example code
    Signed-off-by: José Ulises Niño Rivera <junr03@users.noreply.github.com>

--- envoyfilter/src/lib.rs
+++ envoyfilter/src/lib.rs
@@ -2,9 +2,6 @@ use common_types::EmbeddingRequest;
 use log::info;
 use log::warn;
 use serde_json::to_string;
-use stats::IncrementingMetric;
-use stats::Metric;
-use stats::RecordingMetric;
 use std::collections::HashMap;
 use std::time::Duration;
 
@@ -23,11 +20,7 @@ proxy_wasm::main! {{
         Box::new(FilterContext {
           callouts: HashMap::new(),
           config: None,
-          metrics: WasmMetrics {
-                counter: stats::Counter::new(String::from("wasm_counter")),
-                gauge: stats::Gauge::new(String::from("wasm_gauge")),
-                histogram: stats::Histogram::new(String::from("wasm_histogram")),
-            },
+          metrics: WasmMetrics {},
         })
     });
 }}
@@ -44,70 +37,39 @@ impl HttpContext for StreamContext {
     // Envoy's HTTP model is event driven. The WASM ABI has given implementors events to hook onto
     // the lifecycle of the http request and response.
     fn on_http_request_headers(&mut self, _num_headers: usize, _end_of_stream: bool) -> Action {
-        // Read config
-        info!("config: {:?}", self.config.prompt_config.system_prompt);
-        // Metrics
-        self.metrics.counter.increment(10);
-        info!("counter -> {}", self.metrics.counter.value());
-        self.metrics.gauge.record(20);
-        info!("gauge -> {}", self.metrics.gauge.value());
-        self.metrics.histogram.record(30);
-        // info!("histogram -> {}", self.metrics.histogram.value());
-
-        // Example of reading the HTTP headers on the incoming request
-        for (name, value) in &self.get_http_request_headers() {
-            info!("#{} -> {}: {}", self.context_id, name, value);
-        }
-
         // Save the host header to be used by filter logic later on.
         self.host_header = self.get_http_request_header(":host");
+
         // Remove the Content-Length header because further body manipulations in the gateway logic will invalidate it.
         // Server's generally throw away requests whose body length do not match the Content-Length header.
         // However, a missing Content-Length header is not grounds for bad requests given that intermediary hops could
         // manipulate the body in benign ways e.g., compression.
         self.set_http_request_header("content-length", None);
 
-        // Example logic of branching based on a request header.
         match self.get_http_request_header(":path") {
-            // If the path header is present and the path is /inline
-            Some(path) if path == "/inline" => {
-                // Dispatch an HTTP call inline. This is the model that we will use for the LLM routing host.
-                self.dispatch_http_call(
-                    "httpbin",
-                    vec![
-                        (":method", "GET"),
-                        (":path", "/bytes/1"),
-                        (":authority", "httpbin.org"),
-                    ],
-                    None,
-                    vec![],
-                    Duration::from_secs(5),
-                )
-                .unwrap();
-                // Pause the filter until the out of band HTTP response arrives.
-                Action::Pause
-            }
             // The gateway can start gathering information necessary for routing. For now change the path to an
             // OpenAI API path.
             Some(path) if path == "/llmrouting" => {
                 self.set_http_request_header(":path", Some("/v1/chat/completions"));
-                Action::Continue
             }
             // Otherwise let the filter continue.
-            _ => Action::Continue,
+            _ => (),
         }
+
+        Action::Continue
     }
 
     fn on_http_request_body(&mut self, body_size: usize, end_of_stream: bool) -> Action {
+        // Let the client send the gateway all the data before sending to the LLM_provider.
+        // TODO: consider a streaming API.
+        if !end_of_stream {
+            return Action::Pause;
+        }
+
         // Let the filter continue if the request is not meant for OpenAi
         match &self.host_header {
             Some(host) if host != "api.openai.com" => return Action::Continue,
             _ => {}
-        }
-
-        // Let the client send the gateway all the data before sending to the LLM_provider
-        if !end_of_stream {
-            return Action::Pause;
         }
 
         if let Some(body_bytes) = self.get_http_request_body(0, body_size) {
@@ -131,44 +93,13 @@ impl HttpContext for StreamContext {
 
         Action::Continue
     }
-
-    fn on_http_response_headers(&mut self, _: usize, _: bool) -> Action {
-        self.set_http_response_header("Powered-By", Some("Katanemo"));
-        Action::Continue
-    }
 }
 
-impl Context for StreamContext {
-    // Note that the event driven model continues here from the return of the on_http_request_headers above.
-    fn on_http_call_response(&mut self, _: u32, _: usize, body_size: usize, _: usize) {
-        info!("on_http_call_response: body_size = {}", body_size);
-        if let Some(body) = self.get_http_call_response_body(0, body_size) {
-            if !body.is_empty() && body[0] % 2 == 0 {
-                info!("Access granted.");
-                // This call allows the filter to continue operating on the HTTP request sent by the user.
-                // In Katanemo's use case the call would continue after the LLM host has responded with routing
-                // decisions.
-                self.resume_http_request();
-                return;
-            }
-        }
-        info!("Access forbidden.");
-        // This is an example of short-circuiting the http request and sending back a response to the client.
-        // i.e there was never an external HTTP request made. This could be used for example if the user prompt requires
-        // more information before it can be sent out to a third party API.
-        self.send_http_response(
-            403,
-            vec![("Powered-By", "Katanemo")],
-            Some(b"Access forbidden.\n"),
-        );
-    }
-}
+impl Context for StreamContext {}
 
 #[derive(Copy, Clone)]
 struct WasmMetrics {
-    counter: stats::Counter,
-    gauge: stats::Gauge,
-    histogram: stats::Histogram,
+    // Fill with metrics as needed
 }
 
 struct FilterContext {
