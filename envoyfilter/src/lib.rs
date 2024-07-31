fc02c3060745b7cee2c1bbc8823eb2f8daff201d
José Ulises Niño Rivera <junr03@users.noreply.github.com>
2024-07-31 14:25:23 -0700

    wip
    Signed-off-by: José Ulises Niño Rivera <junr03@users.noreply.github.com>

--- envoyfilter/src/lib.rs
+++ envoyfilter/src/lib.rs
@@ -1,4 +1,8 @@
+use std::collections::HashMap;
+use std::sync::OnceLock;
+
 use filter_context::FilterContext;
+use log::error;
 use proxy_wasm::traits::*;
 use proxy_wasm::types::*;
 
@@ -9,9 +13,16 @@ mod filter_context;
 mod stats;
 mod stream_context;
 
+fn ratelimits() -> &'static HashMap<u32, u32> {
+    static HASHMAP: OnceLock<HashMap<u32, u32>> = OnceLock::new();
+    HASHMAP.get_or_init(|| HashMap::new())
+}
+
 proxy_wasm::main! {{
     proxy_wasm::set_log_level(LogLevel::Trace);
     proxy_wasm::set_root_context(|_| -> Box<dyn RootContext> {
         Box::new(FilterContext::new())
     });
+
+    error!("Only one ratelimit! {:p}", ratelimits());
 }}
