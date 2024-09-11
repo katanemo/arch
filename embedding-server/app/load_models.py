157ae651a8007a62177e770f8132f63fafdc749e
Co Tran <cotran2@utexas.edu>
2024-09-11 23:35:31 +0000

    fix path loading model

--- embedding-server/app/load_models.py
+++ embedding-server/app/load_models.py
@@ -25,7 +25,7 @@ def load_toxic_model(model_name = os.getenv("TOXIC_MODELS", "katanemolabs/toxic-
     toxic_model = {}
     toxic_model['tokenizer'] = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
     toxic_model['model_name'] = model_name
-    toxic_model['model'] = ort.InferenceSession(model_name, opts, providers=["CPUExecutionProvider"])
+    toxic_model['model'] = ort.InferenceSession(model_name + '/model_quantized.onnx', opts, providers=["CPUExecutionProvider"])
     toxic_model['positive_class'] = 1
     
     return toxic_model
@@ -36,7 +36,7 @@ def load_jailbreak_model(model_name = os.getenv("JAILBREAK_MODELS", "katanemolab
     jailbreak_model = {}
     jailbreak_model['tokenizer'] = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
     jailbreak_model['model_name'] = model_name
-    jailbreak_model['model'] = ort.InferenceSession(model_name, opts, providers=["CPUExecutionProvider"])
+    jailbreak_model['model'] = ort.InferenceSession(model_name + '/model_quantized.onnx', opts, providers=["CPUExecutionProvider"])
     jailbreak_model['positive_class'] = 2
     
     return jailbreak_model
