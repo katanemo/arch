b8841fb8f49db22096482ccd580bc8b0ef04fcbf
Adil Hafeez <adil@katanemo.com>
2024-09-17 08:56:47 -0700

    rename bolt-config => bolt_config

--- config_generator/config_generator.py
+++ config_generator/config_generator.py
@@ -2,7 +2,7 @@ import os
 from jinja2 import Environment, FileSystemLoader
 
 ENVOY_CONFIG_TEMPLATE_FILE = os.getenv('ENVOY_CONFIG_TEMPLATE_FILE', 'envoy.template.yaml')
-BOLT_CONFIG_FILE = os.getenv('BOLT_CONFIG_FILE', 'bolt-config.yaml')
+BOLT_CONFIG_FILE = os.getenv('BOLT_CONFIG_FILE', 'bolt_config.yaml')
 ENVOY_CONFIG_FILE_RENDERED = os.getenv('ENVOY_CONFIG_FILE_RENDERED', '/usr/src/app/out/envoy.yaml')
 
 env = Environment(loader=FileSystemLoader('./'))
