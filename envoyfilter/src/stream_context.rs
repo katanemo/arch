ce6b3d808a4b9f67f785e4a343bebe1a0335e64a
Adil Hafeez <adil@katanemo.com>
2024-09-24 12:33:42 -0700

    update format

--- envoyfilter/src/stream_context.rs
+++ envoyfilter/src/stream_context.rs
@@ -515,7 +515,7 @@ impl StreamContext {
             Ok(token_id) => token_id,
             Err(e) => {
                 let error_msg = format!(
-                    "Error dispatching call cluster: {}, path: {}, err: {:?}",
+                    "Error dispatching call to cluster: {}, path: {}, err: {:?}",
                     &endpoint.cluster, path, e
                 );
                 return self.send_server_error(error_msg, Some(StatusCode::BAD_REQUEST));
