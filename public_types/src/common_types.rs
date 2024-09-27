069ee7dff0702b3430dee67e56afeb31441c7c7a
Adil Hafeez <adil@katanemo.com>
2024-09-27 10:53:15 -0700

    fix more

--- public_types/src/common_types.rs
+++ public_types/src/common_types.rs
@@ -175,9 +175,11 @@ pub mod open_ai {
     #[derive(Debug, Clone, Serialize, Deserialize)]
     pub struct Message {
         pub role: String,
+        #[serde(skip_serializing_if = "Option::is_none")]
         pub content: Option<String>,
         #[serde(skip_serializing_if = "Option::is_none")]
         pub model: Option<String>,
+        #[serde(skip_serializing_if = "Option::is_none")]
         pub tool_calls: Option<Vec<ToolCall>>,
     }
 
