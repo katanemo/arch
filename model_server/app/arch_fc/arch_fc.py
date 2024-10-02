fea84c769814bef8764f98d24a8a49c582e23fc8
Co Tran <cotran2@utexas.edu>
2024-10-01 18:36:00 -0700

    Update arch_fc.py

--- model_server/app/arch_fc/arch_fc.py
+++ model_server/app/arch_fc/arch_fc.py
@@ -34,7 +34,7 @@ else:
 if mode == "cloud":
     client = OpenAI(
         base_url=fc_url,
-        api_key="vllm",
+        api_key="EMPTY",
     )
     models = client.models.list()
     model = models.data[0].id
