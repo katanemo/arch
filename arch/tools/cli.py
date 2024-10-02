41183aede4b83da1cdde48f82da08649e8f56941
Salman Paracha <salmanparacha@MacBook-Pro-261.local>
2024-10-01 22:08:48 -0700

    committing the cli

--- arch/tools/cli.py
+++ arch/tools/cli.py
@@ -1,36 +1,54 @@
 import click
 from core import start_arch
-from targets import generate_prompt_targets
+import targets
 
-@click.group()
-def main():
-    """CLI to manage Arch - The Intelligent Prompt Gateway."""
-    pass
+logo = r"""
+     _                _
+    / \    _ __  ___ | |__
+   / _ \  | '__|/ __|| '_ \
+  / ___ \ | |  | (__ | | | |
+ /_/   \_\|_|   \___||_| |_|
+
+"""
+@click.group(invoke_without_command=True)
+@click.pass_context
+def main(ctx):
+    if ctx.invoked_subcommand is None:
+        click.echo( """Arch (The Intelligent Prompt Gateway) CLI""")
+        click.echo(logo)
+        click.echo(ctx.get_help())
 
 @click.command()
-@click.option('--dir', default='.', help='Path to the directory containing prompt_config.yml')
-def up(dir):
-    """Reads prompt_config.yml and starts up Arch using Docker."""
-    start_arch(dir)  # Placeholder for Docker interaction
+@click.option('--path', default='.', help='Path to the directory containing prompt_config.yml')
+def up(path):
+    """Starts Arch."""
+    start_arch(path)
+
+@click.command()
+@click.option('--path', default='.', help='Path to the directory containing prompt_config.yml')
+def down(path):
+    """Stops Arch."""
+    stop_arch(path)
 
 @click.command()
 @click.argument('file', type=click.Path(exists=True))
-def generate_targets_python(file):
-    """Reads a Python file and generates prompt_targets.yml. 
-       Note: This works for simple data types like ['int', 'float', 'bool', 'str', 'list', 'tuple', 'set', 'dict']: 
+def generate_prompt_targets(file):
+    """Generats prompt_targets from python methods.
+       Note: This works for simple data types like ['int', 'float', 'bool', 'str', 'list', 'tuple', 'set', 'dict']:
        If you have a complex pydantic data type, you will have to flatten those manually until we add support for it."""
-    
+
     if file.endswith(".py"):
         output_file = file.replace(".py", "_prompt_targets.yml")
     else:
         print("Error: Input file must be a .py file")
         sys.exit(1)
-        
-    generate_prompt_targets(file, output_file)
+
+    targets.generate_prompt_targets(file, output_file)
     print(f"Sucessfully generated targets file: {output_file}")
 
 main.add_command(up)
-main.add_command(generate_targets_python)
+main.add_command(down)
+main.add_command(generate_prompt_targets)
 
 if __name__ == '__main__':
     main()
--- arch/tools/setup.py
+++ arch/tools/setup.py
@@ -6,8 +6,11 @@ setup(
     description="Python-based CLI tool to manage Arch and generate targets.",
     author="Katanemo Labs, Inc.",
     packages=find_packages(),
-    py_modules = ['cli', 'core', 'targets'],
+    py_modules = ['cli', 'core', 'targets', 'utils'],
     include_package_data=True,
+    package_data={
+        '': ['../docker-compose.yml'],  # Specify to include the docker-compose.yml file
+    },
     install_requires=['pyyaml', 'pydantic', 'click'],  # Add dependencies here, e.g., 'PyYAML' for YAML processing
     entry_points={
         'console_scripts': [
