412712d35663570fba0f8368029827a33834cc98
Salman Paracha <salmanparacha@MacBook-Pro-261.local>
2024-10-07 10:40:36 -0700

    updated docker build command to run as local user

--- docs/build_docs.sh
+++ docs/build_docs.sh
@@ -1,4 +1,4 @@
 docker build -f Dockerfile . -t sphinx
-docker run --rm -v $(pwd):/docs sphinx make clean
-docker run --rm -v $(pwd):/docs sphinx make html
+docker run --user $(id -u):$(id -g) --rm -v $(pwd):/docs sphinx make clean
+docker run --user $(id -u):$(id -g) --rm -v $(pwd):/docs sphinx make html
 chmod -R 777 build/html
