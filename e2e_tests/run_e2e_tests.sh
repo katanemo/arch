1a2f9280edb81852d19c632f3d1959763bbe979b
Adil Hafeez <adil@katanemo.com>
2024-10-26 15:19:29 -0700

    more fixes

--- e2e_tests/run_e2e_tests.sh
+++ e2e_tests/run_e2e_tests.sh
@@ -6,7 +6,18 @@ pwd
 
 . ./common_scripts.sh
 
+log building llm and prompt gateway rust modules
+log ============================================
+cd ../arch
+sh build_filter_image.sh
+log starting the arch gateway service
+docker compose -f docker-compose.yaml down
+docker compose -f docker-compose.yaml up -d
+wait_for_healthz "http://localhost:10000/healthz" 60
+cd -
+
 log building function_callling demo
+log ===============================
 cd ../demos/function_calling
 docker compose build
 
@@ -15,32 +26,26 @@ docker compose up -d
 cd -
 
 log building model server
+log =====================
 cd ../model_server
 poetry install
 log starting model server
 poetry run archgw_modelserver restart
 cd -
 
-log building llm and prompt gateway rust modules
-cd ../arch
-sh build_filter_image.sh
-log starting the arch gateway service
-docker compose -f docker-compose.yaml down
-docker compose -f docker-compose.yaml up -d
-cd -
-
-wait_for_healthz "http://localhost:10000/healthz" 60
-
 log running e2e tests
+log =================
 poetry install
 poetry run pytest
 
 log shutting down the arch gateway service
+log ======================================
 cd ../arch
 docker compose -f docker-compose.yaml stop
 cd -
 
 log shutting down the function_calling demo
+log =======================================
 cd ../demos/function_calling
 docker compose down
 cd -
