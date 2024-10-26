963bc4ac5ac5969403cca2286746b36be44ac05a
Adil Hafeez <adil@katanemo.com>
2024-10-26 14:29:46 -0700

    replace pushd with cd

--- e2e_tests/run_e2e_tests.sh
+++ e2e_tests/run_e2e_tests.sh
@@ -7,28 +7,28 @@ pwd
 . ./common_scripts.sh
 
 log building function_callling demo
-pushd ../demos/function_calling
+cd ../demos/function_calling
 docker compose build
 
 log starting the function_calling demo
 docker compose up -d
-popd
+cd -
 
 log building model server
-pushd ../model_server
+cd ../model_server
 poetry install
 log starting model server
 archgw_modelserver restart
-popd
+cd -
 
 log building llm and prompt gateway rust modules
-pushd ../arch
+cd ../arch
 sh build_filter_image.sh
 log starting the arch gateway service
 ARCH_CONFIG_FILE=../demos/function_calling/arch_config.yaml
 docker compose -f docker-compose.dev.yaml down
 docker compose -f docker-compose.dev.yaml up -d
-popd
+cd -
 
 wait_for_healthz "http://localhost:10000/healthz" 60
 
@@ -37,11 +37,11 @@ poetry install
 poetry run pytest
 
 log shutting down the arch gateway service
-pushd ../arch
+cd ../arch
 docker compose -f docker-compose.dev.yaml stop
-popd
+cd -
 
 log shutting down the function_calling demo
-pushd ../demos/function_calling
+cd ../demos/function_calling
 docker compose down
-popd
+cd -
