6a16c793d88be0949d207e4f0e63ec89cbdda4bf
Adil Hafeez <adil@katanemo.com>
2024-10-28 14:19:45 -0700

    better names

--- e2e_tests/run_e2e_tests.sh
+++ e2e_tests/run_e2e_tests.sh
@@ -33,6 +33,7 @@ cd ../model_server
 poetry install 2>&1 >> ../build.log
 log starting model server
 log =====================
+touch ~/archgw_logs/modelserver.log
 poetry run archgw_modelserver restart &
 tail -F ~/archgw_logs/modelserver.log &
 model_server_tail_pid=$!
