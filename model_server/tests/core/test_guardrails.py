b7c4a1076b3fbadbd57f9a884f35a73c47b17325
cotran <cotran2@utexas.edu>
2024-12-18 13:34:06 -0800

    fix test

--- model_server/tests/core/test_guardrails.py
+++ model_server/tests/core/test_guardrails.py
@@ -12,19 +12,17 @@ arch_guard_model_type = {
 # [TODO] Review: check the following code to test under `cpu`, `cuda`, and `mps`
 # Test for `get_guardrail_handler()` function on `cpu`
 @patch("src.core.guardrails.AutoTokenizer.from_pretrained")
-@patch("src.core.guardrails.OVModelForSequenceClassification.from_pretrained")
 @patch("src.core.guardrails.AutoModelForSequenceClassification.from_pretrained")
-def test_guardrail_handler_on_cpu(mock_auto_model, mock_ov_model, mock_tokenizer):
+def test_guardrail_handler_on_cpu(mock_auto_model, mock_tokenizer):
     device = "cpu"
 
-    mock_ov_model.return_value = MagicMock()
     mock_tokenizer.return_value = MagicMock()
 
     guardrail = get_guardrail_handler(device=device)
 
     mock_tokenizer.assert_called_once_with(guardrail.model_name, trust_remote_code=True)
 
-    mock_ov_model.assert_called_once_with(
+    mock_auto_model.assert_called_once_with(
         guardrail.model_name,
         device_map=device,
         low_cpu_mem_usage=True,
@@ -33,9 +31,8 @@ def test_guardrail_handler_on_cpu(mock_auto_model, mock_ov_model, mock_tokenizer
 
 # Test for `get_guardrail_handler()` function on `cuda`
 @patch("src.core.guardrails.AutoTokenizer.from_pretrained")
-@patch("src.core.guardrails.OVModelForSequenceClassification.from_pretrained")
 @patch("src.core.guardrails.AutoModelForSequenceClassification.from_pretrained")
-def test_guardrail_handler_on_cuda(mock_auto_model, mock_ov_model, mock_tokenizer):
+def test_guardrail_handler_on_cuda(mock_auto_model, mock_tokenizer):
     device = "cuda"
 
     mock_auto_model.return_value = MagicMock()
@@ -54,9 +51,8 @@ def test_guardrail_handler_on_cuda(mock_auto_model, mock_ov_model, mock_tokenize
 
 # Test for `get_guardrail_handler()` function on `mps`
 @patch("src.core.guardrails.AutoTokenizer.from_pretrained")
-@patch("src.core.guardrails.OVModelForSequenceClassification.from_pretrained")
 @patch("src.core.guardrails.AutoModelForSequenceClassification.from_pretrained")
-def test_guardrail_handler_on_mps(mock_auto_model, mock_ov_model, mock_tokenizer):
+def test_guardrail_handler_on_mps(mock_auto_model, mock_tokenizer):
     device = "mps"
 
     mock_auto_model.return_value = MagicMock()
